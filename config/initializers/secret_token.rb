4e0dea8e4f341a4e11de8dd25581c89880ef66c3
dereky <dereky@gmail.com>
2011-05-01 18:42:31 -0400

    changed homepage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dereky::Application.config.secret_token = '5409643e1fbe484eee6f00bb32429fe513b10d23a0c58f287a91514c3c97c07910535513890728e71478db724d856151364fcb1a42e03155780d462c2ebbbaae'
